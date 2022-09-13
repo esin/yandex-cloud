a550b4ced95d29e910c62ef682260215e31cb6f4
Andrey Esin <gmlastik@gmail.com>
2022-09-13 13:13:16 +0300

    Init commit

---
+++ serverless-containers/dummy/runserver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm /tmp/fifo || true
+mkfifo /tmp/fifo
+LISTENIP=127.0.0.1
+cat /tmp/fifo | nc -lvk4N $LISTENIP $PORT &
+echo "netcat server started"
+
+
+
+#### Here your script
+sleep 0.5;
+curl https://api.github.com/octocat
+#### Here your script
+
+
+
+### After everything is done
+echo -e 'HTTP/1.1 200\r\nConnection: close\r\n\r\n\r\nEnjoy the silence' > /tmp/fifo
+while true; do
+    sleep 0.5
+done
+
+exit 0
