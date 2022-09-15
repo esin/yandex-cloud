822710518120d12ad8662f009b2311de2339a8c5
Andrey Esin <gmlastik@gmail.com>
2022-09-15 11:41:46 +0300

    Добавлен README Добавлена возможность работы с элементами HTTP

--- serverless-containers/dummy/runserver.sh
+++ serverless-containers/dummy/runserver.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 ### Need to run netcat server
-rm /tmp/http.response || true
+rm /tmp/http.response 2> /dev/null || true
 mkfifo /tmp/http.response
 LISTENIP=127.0.0.1
 cat /tmp/http.response | nc -lvk4N $LISTENIP $PORT > /tmp/http.request &
 echo "netcat server started"
+sleep 0.5
 ### Need to run netcat server
+
+### Some useful info about http request
+HTTP_REQUEST_METHOD=$(cat /tmp/http.request | head -n1 | cut -f1 -d' ')
+HTTP_REQUEST_PATH=$(dirname `cat /tmp/http.request | head -n1 | cut -f2 -d' '`)
+HTTP_REQUEST_PARAMS=$(cat /tmp/http.request | head -n1 | cut -f2 -d' ' | cut -f2 -d'?')
+echo "HTTP Method: $HTTP_REQUEST_METHOD"
+echo "HTTP request path: $HTTP_REQUEST_PATH"
+echo "HTTP request params: $HTTP_REQUEST_PARAMS"
+### Some useful info about http request
 
 
 
@@ -15,8 +25,6 @@ sleep 0.5;
 curl https://api.github.com/octocat
 #### Here your script
 
-echo "OUT: "
-cat /tmp/http.request
 
 
 ### After everything is done
