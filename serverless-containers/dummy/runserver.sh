13895bccc4d5ce721ca6323431c3c505373d6dbf
Andrey Esin <gmlastik@gmail.com>
2022-09-15 10:46:14 +0300

    Naming++

--- serverless-containers/dummy/runserver.sh
+++ serverless-containers/dummy/runserver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ### Need to run netcat server
-rm /tmp/fifo || true
-mkfifo /tmp/fifo
+rm /tmp/http.response || true
+mkfifo /tmp/http.response
 LISTENIP=127.0.0.1
-cat /tmp/fifo | nc -lvk4N $LISTENIP $PORT &
+cat /tmp/http.response | nc -lvk4N $LISTENIP $PORT > /tmp/http.request &
 echo "netcat server started"
 ### Need to run netcat server
 
@@ -15,10 +15,12 @@ sleep 0.5;
 curl https://api.github.com/octocat
 #### Here your script
 
+echo "OUT: "
+cat /tmp/http.request
 
 
 ### After everything is done
-echo -e 'HTTP/1.1 200\r\nConnection: close\r\n\r\n\r\nEnjoy the silence' > /tmp/fifo
+echo -e 'HTTP/1.1 200\r\nConnection: close\r\n\r\n\r\nEnjoy the silence' > /tmp/http.response
 while true; do
     sleep 0.5
 done
